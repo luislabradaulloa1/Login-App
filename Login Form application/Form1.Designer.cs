4eabeb9c3ced0a7c93cb13b01a4cc2fa63d9f498
luislabradaulloa1 <luis.labrada.ulloa@gmail.com>
2026-01-20 19:48:32 -0300

    Add files via upload

---
+++ Login Form application/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Login_Form_application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.usernameValue = new System.Windows.Forms.TextBox();
+            this.passwordValue = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.showPasswordCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // headerLabel
+            // 
+            resources.ApplyResources(this.headerLabel, "headerLabel");
+            this.headerLabel.Name = "headerLabel";
+            // 
+            // usernameLabel
+            // 
+            resources.ApplyResources(this.usernameLabel, "usernameLabel");
+            this.usernameLabel.Name = "usernameLabel";
+            // 
+            // passwordLabel
+            // 
+            resources.ApplyResources(this.passwordLabel, "passwordLabel");
+            this.passwordLabel.Name = "passwordLabel";
+            // 
+            // usernameValue
+            // 
+            this.usernameValue.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.usernameValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.usernameValue, "usernameValue");
+            this.usernameValue.Name = "usernameValue";
+            // 
+            // passwordValue
+            // 
+            this.passwordValue.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.passwordValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.passwordValue, "passwordValue");
+            this.passwordValue.Name = "passwordValue";
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.loginButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.loginButton, "loginButton");
+            this.loginButton.Name = "loginButton";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.clearButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.clearButton, "clearButton");
+            this.clearButton.Name = "clearButton";
+            this.clearButton.UseVisualStyleBackColor = false;
+            this.clearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.exitButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.exitButton, "exitButton");
+            this.exitButton.Name = "exitButton";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // showPasswordCheckBox
+            // 
+            resources.ApplyResources(this.showPasswordCheckBox, "showPasswordCheckBox");
+            this.showPasswordCheckBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.showPasswordCheckBox.Name = "showPasswordCheckBox";
+            this.showPasswordCheckBox.UseVisualStyleBackColor = true;
+            this.showPasswordCheckBox.CheckedChanged += new System.EventHandler(this.ShowPasswordChanged);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Controls.Add(this.showPasswordCheckBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordValue);
+            this.Controls.Add(this.usernameValue);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.headerLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label headerLabel;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox usernameValue;
+        private System.Windows.Forms.TextBox passwordValue;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.CheckBox showPasswordCheckBox;
+    }
+}
+
---
+++ Login Form application/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Login_Form_application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-8MAP9P3\SQLEXPRESS;Initial Catalog=MyDatabase;Integrated Security=True;Encrypt=False");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            String username, password;
+
+            username = usernameValue.Text;
+            password = passwordValue.Text;
+
+            try
+            {
+                string querry = "SELECT * FROM Login_new WHERE username = '"+usernameValue.Text+"' AND password = '"+passwordValue.Text+"'";
+                SqlDataAdapter sda = new SqlDataAdapter(querry, conn);
+
+                DataTable dtable = new DataTable();
+                sda.Fill(dtable);
+
+                if (dtable.Rows.Count > 0)
+                {
+                    username = usernameValue.Text;
+                    password = passwordValue.Text;
+
+                    MenuForm form2 = new MenuForm();
+                    form2.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid login details", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    usernameValue.Clear();
+                    passwordValue.Clear();
+
+                    usernameValue.Focus();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("An error occurred while trying to log in.");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            usernameValue.Clear();
+            passwordValue.Clear();
+
+            usernameValue.Focus();
+        }
+
+        private void ShowPasswordChanged(object sender, EventArgs e)
+        {
+            passwordValue.PasswordChar = showPasswordCheckBox.Checked ? '\0' : '*';
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
